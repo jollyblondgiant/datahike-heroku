4db1f9ef7be956671b5de5f4a339270a8a44e4a5
jollyblondgiant <andrew.carlil3@gmail.com>
2020-12-29 10:22:38 -0500

    begin debuging

--- project.clj
+++ project.clj
@@ -1,2 +1,13 @@
 ;; emtpy placeholder to make heroku recognize it as a clojure project
-;; https://devcenter.heroku.com/articles/clojure-support#activation
+;; https://devcenter.heroku.com/articles/clojure-support#activatio
+(defproject example-project "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [io.replikativ/datahike "0.3.2"]
+                 [io.replikativ/datahike-postgres "0.1.0"]]
+   :profiles
+   {:dev
+    {:dependencies [[org.clojure/clojurescript "1.10.773"]
+                    [com.bhauman/figwheel-main "0.2.12"]
+                    ;; optional but recommended
+                    [com.bhauman/rebel-readline-cljs "0.1.4"]]}})
